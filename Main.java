73e1593dc0ab26c0f3a67458db6fa0b9616e8340
Julia81-pr <75021260+Julia81-pr@users.noreply.github.com>
2020-12-15 23:37:30 +0300

    Исправления
    Исправлена формула подсчета и названия переменных

--- Main.java
+++ Main.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        CreditPaymentService credit = new CreditPaymentService();
-        Scanner input = new Scanner(System.in);
-        System.out.println("Введите сумму кредита: ");
-        double summa= input.nextDouble();
 
-        System.out.println("Введите количество месяцев: ");
-        double month= input.nextDouble();
+            public static void main(String[] args) {
 
-        System.out.println("Введите размер процентной ставки: ");
-        double rate = input.nextDouble();
+            Scanner input = new Scanner(System.in);
+            double PERCENTS = 2.2;
+            // System.out.println("сумма кредита ");
+            double credit = 5000;//input.nextInt();
+            // System.out.println("первый взнос ");
+            double firstPayment = 1000;// input.nextInt();
+            // System.out.println("Срок ");
+            double paymentTerm = 12;//input.nextInt();
+            double leftToPay = credit - firstPayment;
+            double Z = PERCENTS / 100 / paymentTerm;
+            double linearPercent = leftToPay * Z;
+            System.out.format("Линейный процент %.2f\n", linearProcent);
+            double payPerMonth = linearProcent + (leftToPay / paymentTerm);
 
-        double pay = credit.calculate(summa, month, rate);
+            for (double i = leftToPay; i > 0; i -= payPerMonth) {
+
+                payPerMonth= linearProcent + (leftToPay / paymentTerm);//340.67
 
 
-        for (int i = 0; i<11; i++){
-            System.out.println("Сумма платежа  " + pay);
+                System.out.format("Проц %-6.2fМес %-8.2fОст %-8.2f\n", linearProcent, payPerMonth, i);
+                linearProcent -= linearProcent - (leftToPay - payPerMonth) * Z;
+
+            }
         }
-
-
     }
 
-
-
-}
